72395708e232d397daacdc186a3af4c731f39ee2
Youngji <youngji.brigid.kim@gmail.com>
2020-05-08 15:16:49 -0400

    debugging

--- loss.py
+++ loss.py
@@ -85,7 +85,7 @@ class ImageBasedCrossEntropyLoss2d(nn.Module):
                 weights = self.calculate_weights(target_cpu[i])
                 self.nll_loss.weight = torch.Tensor(weights).cuda()
 
-            print(weights)
+#            print(weights)
 #            print(target_cpu[i].min())
 #            print(target_cpu[i])
             loss1 = self.nll_loss(F.log_softmax(inputs[i].unsqueeze(0)),targets[i].unsqueeze(0))
