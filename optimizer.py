9da59c55444da25d5d132f6e69864bf7868be151
Youngji <youngji.brigid.kim@gmail.com>
2020-05-08 21:31:55 -0400

    load one ckpts

--- optimizer.py
+++ optimizer.py
@@ -73,10 +73,10 @@ def restore_snapshot(net, optimizer, snapshot, snapshot2, restore_optimizer_bool
         optimizer.load_state_dict(checkpoint['optimizer'])
 
 
-    if 'state_dict' in checkpoint2:
-        net = forgiving_state_restore(net, checkpoint2['state_dict'])
-    else:
-        net = forgiving_state_restore(net, checkpoint2)
+#    if 'state_dict' in checkpoint2:
+#        net = forgiving_state_restore(net, checkpoint2['state_dict'])
+#    else:
+#        net = forgiving_state_restore(net, checkpoint2)
 
     if 'state_dict' in checkpoint:
         net = forgiving_state_restore(net, checkpoint['state_dict'])
