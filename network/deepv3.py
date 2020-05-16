83fea34cd303b64d820a2052c1298b567b9f9807
Youngji <youngji.brigid.kim@gmail.com>
2020-05-16 15:22:00 -0400

    modify load dict

--- network/deepv3.py
+++ network/deepv3.py
@@ -522,16 +522,28 @@ class DeepWV3Plus(nn.Module):
 
         tasks = ['semantic', 'traversability']
         wide_resnet = wider_resnet38_a2(classes=1000, dilation=True, tasks=tasks)
-#        wide_resnet = torch.nn.DataParallel(wide_resnet)
-#        if criterion is not None:
-#            try:
-#                checkpoint = torch.load('./pretrained_models/wider_resnet38.pth.tar', map_location='cpu')
+        wide_resnet = torch.nn.DataParallel(wide_resnet)
+        if criterion is not None:
+            try:
+                checkpoint = torch.load('./pretrained_models/wider_resnet38.pth.tar', map_location='cpu')
 #                wide_resnet.load_state_dict(checkpoint['state_dict'])
-#                del checkpoint
-#            except:
-#                print("Please download the ImageNet weights of WideResNet38 in our repo to ./pretrained_models/wider_resnet38.pth.tar.")
-#                raise RuntimeError("=====================Could not load ImageNet weights of WideResNet38 network.=======================")
-#        wide_resnet = wide_resnet.module
+
+                net_state_dict = wide_resnet.state_dict()
+                loaded_dict = checkpoint['state_dict']
+                new_loaded_dict = {}
+                for k in net_state_dict:
+                    if k in loaded_dict and net_state_dict[k].size() == loaded_dict[k].size():
+                        new_loaded_dict[k] = loaded_dict[k]
+                    else:
+                        logging.info("Skipped loading parameter %s", k)
+                net_state_dict.update(new_loaded_dict)
+                wide_resnet.load_state_dict(net_state_dict)
+
+                del checkpoint
+            except:
+                print("Please download the ImageNet weights of WideResNet38 in our repo to ./pretrained_models/wider_resnet38.pth.tar.")
+                raise RuntimeError("=====================Could not load ImageNet weights of WideResNet38 network.=======================")
+        wide_resnet = wide_resnet.module
 
 
         
