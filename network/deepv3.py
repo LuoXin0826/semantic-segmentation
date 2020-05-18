328927533fe2d3a776ec97a5125e58511a24189c
Youngji <youngji.brigid.kim@gmail.com>
2020-05-18 19:16:23 -0400

    .

--- network/deepv3.py
+++ network/deepv3.py
@@ -714,7 +714,7 @@ class DeepWV3Plus(nn.Module):
         dec0_up = Upsample(dec0_up, m2.size()[2:])
         dec0 = [dec0_fine, dec0_up]
         dec0 = torch.cat(dec0, 1)
-        dec1 = self.final(dec0)
+        dec1 = self.final2(dec0)
         out2 = Upsample(dec1, x_size[2:])
 
 #        dec1 = self.final2(dec0)
