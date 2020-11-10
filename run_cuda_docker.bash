461425dbb38f52a5b88e73d47e1c9aa6046fe5b9
LuoXin0826 <luoxin@umich.edu>
2020-11-10 13:03:11 -0500

    update bash file

--- run_cuda_docker.bash
+++ run_cuda_docker.bash
@@ -1,5 +1,20 @@
 container_name=$1
 
 xhost +local:
-docker container run --rm -it nvidia-segmentation:latest
+docker run -it --net=host --runtime=nvidia \
+  --user=$(id -u) \
+  -e DISPLAY=$DISPLAY \
+  -e QT_GRAPHICSSYSTEM=native \
+  -e CONTAINER_NAME=cuda \
+  -e USER=$USER \
+  --workdir=/home/$USER \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v "/etc/group:/etc/group:ro" \
+  -v "/etc/passwd:/etc/passwd:ro" \
+  -v "/etc/shadow:/etc/shadow:ro" \
+  -v "/etc/sudoers.d:/etc/sudoers.d:ro" \
+  -v "/home/$USER/:/home/$USER/" \
+  --device=/dev/dri:/dev/dri \
+  --name=${container_name} \
+  nvidia-segmentation:latest
 
