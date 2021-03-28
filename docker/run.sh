fae50853bf48853d2a117da2086d01def89940e1
LuoXin0826 <luoxin@umich.edu>
2021-03-28 15:06:29 -0400

    modified docker run command

--- docker/run.sh
+++ docker/run.sh
@@ -5,4 +5,5 @@ sudo docker run --runtime nvidia -it --rm --network host -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /etc/enctune.conf:/etc/enctune.conf \
+    -v /mnt/:/mnt/ \
     jetson-inference:r
