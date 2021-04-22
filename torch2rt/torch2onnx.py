15e81ec59bfc96dd608743e0242f2a1951dda72d
LuoXin0826 <luoxin@umich.edu>
2021-04-22 19:56:56 -0400

    modified model

--- torch2rt/torch2onnx.py
+++ torch2rt/torch2onnx.py
@@ -54,8 +54,6 @@ print('running on device ' + str(device))
 # load the model checkpoint
 print('loading checkpoint:  ' + opt.input)
 checkpoint = torch.load(opt.input)
-print('*******************')
-print(type(checkpoint))
 
 num_classes = opt.num_classes
 
@@ -64,6 +62,7 @@ print('num classes:  ' + str(num_classes))
 
 criterion = CrossEntropyLoss2d(size_average=True, ignore_index=None).cuda()
 model = network.get_net_ori(opt, criterion)
+model = nn.DataParallel(model)
 
 # load the model weights
 model.load_state_dict(checkpoint)
